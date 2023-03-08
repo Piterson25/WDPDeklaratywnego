49f8f2e7664f8511f4e1a05fb0cf2ec7f667280e
Piterson25 <piotrmaszczak25@gmail.com>
2023-03-08 22:45:02 +0100

    Zad 12

--- lab03/zad12.scm
+++ lab03/zad12.scm
@@ -1,5 +1,6 @@
 #lang scheme
-"11"
+
+"12"
 
 (define (filter-accumulate combiner null-value term a next b pred)
   (if (> a b)
@@ -7,14 +8,36 @@
       (let ((next-a (next a)))
         (if (pred a)
             (combiner (term a)
-                      (filter-accumulate combiner null-value term next-a next b pred))
-            (filter-accumulate combiner null-value term next-a next b pred)))))
+                      (filter-accumulate combiner null-value term (next a) next b pred))
+            (filter-accumulate combiner null-value term (next a) next b pred)))))
 
-(define (nwd a b) (cond ((= a b) a)
-                        ((> a b) (nwd (- a b) b))
-                        (else (nwd a (- b a)))
-                  ))
+(define (prime? n)
+  (define (iter i)
+    (cond ((< i 2) #f)
+          ((= i 2) #t)
+          ((even? i) #f)
+          (else (let ((sqrt-n (floor (sqrt n))))
+                  (if (> i sqrt-n)
+                      #t
+                      (if (= 0 (modulo n i))
+                          #f
+                          (iter (+ i 2))))))))
+  (iter n))
 
-(define (square x y)
-  (* x y))
+(define (sum-of-prime-squares a b)
+  (filter-accumulate + 0 (lambda (x) (* x x)) a (lambda (x) (+ x 1)) b prime?))
 
+
+(define (nwd a b)
+  (if (= b 0)
+      a
+      (nwd b (modulo a b))))
+
+(define (coprime? a b)
+  (= (nwd a b) 1))
+
+(define (product-of-coprime n)
+  (filter-accumulate * 1 (lambda (x) x) 1 (lambda (x) (+ x 1)) n (lambda (i) (coprime? i n))))
+
+(sum-of-prime-squares 5 9)
+(product-of-coprime 7)
