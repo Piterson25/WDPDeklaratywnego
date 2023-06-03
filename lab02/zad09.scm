6f438c91ae2054865deb1700b50148e5301ee927
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 18:24:41 +0200

    zad09

--- lab02/zad09.scm
+++ lab02/zad09.scm
@@ -3,7 +3,7 @@
 (define (new-if condition consequence alternative)
    (cond (condition consequence)
          (else      alternative)))
-"In this example even if condition is met, the alternative"
-"is also evaluated"
 
-(new-if (= 1 1) (display "One is equal to one") (/ 1 0))
+;; cond oblicza wynik, zanim sprawdzi warunek
+;; if najpierw sprawdza warunek, a dopiero potem wynik
+;; taki kod nie spełnia zasady condition-consequence/alternative
