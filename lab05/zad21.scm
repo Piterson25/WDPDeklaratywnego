aa7adc243091919cf69cdd9a9c6f1cdfce84bf6f
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 21:09:13 +0200

    zad21

--- lab05/zad21.scm
+++ lab05/zad21.scm
@@ -1,27 +1,35 @@
 #lang scheme
 
-"21."
-
 (define (eval expression values)
-  (let ((typ (car expression)))
-    (cond ((eq? typ 'and)
-           (and (eval (cadr expression) values)
-                (eval (caddr expression) values)))
-          ((eq? typ 'or)
-           (or (eval (cadr expression) values)
-               (eval (caddr expression) values)))
-          ((eq? typ 'not)
-           (not (eval (cadr expression) values)))
-          ((eq? typ 'variable)
-           (let ((var (cadr expression)))
-             (cdr (assoc var values))))
-          ((eq? typ 'constant)
-           (cadr expression)))))
+  (cond ((boolean? expression) expression)
+        ((pair? expression)
+         (let ((operator (car expression))
+               (operands (cdr expression)))
+           (cond ((eq? operator 'and)
+                  (andmap (lambda (operand) (eval operand values)) operands))
+                 ((eq? operator 'or)
+                  (ormap (lambda (operand) (eval operand values)) operands))
+                 ((eq? operator 'not)
+                  (not (eval (car operands) values)))
+                 (else (error "Unknown operator" operator)))))
+        (else (find-value values expression))))
+
+(define (find-value values var)
+  (cond ((boolean? var) var)
+        ((eq? (caar values) var) (cdar values))
+        (else (find-value (cdr values) var))))
+
+(define (calculate-double val1 val2 operation)
+  (if (eq? operation 'and)
+      (and val1 val2)
+      (or val1 val2)))
+
+(define (calculate-single val)
+  (not val))
 
 (define values '((x . #f) (y . #t) (z . #f)))
 
-(eval '(and y (not x)) values)
+(displayln (eval '(and y (not x)) values))
+(displayln (eval '(and y (and (not x) z)) values))
+(displayln (eval '(and y #t) values))
 
-(eval '(and y (and (not x) z)) values)
-
-(eval '(and y #t) values)
