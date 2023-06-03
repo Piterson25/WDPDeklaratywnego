b17ab2d44d72f6b51f4c6c0bbc8a24788429f42f
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 19:59:05 +0200

    zad15

--- lab04/zad15.scm
+++ lab04/zad15.scm
@@ -1,15 +1,15 @@
 #lang scheme
 
-"15."
-
 (define (comb f g)
    (lambda (x) (f (g x))))
 
-(define (square n) (* n n))
+(define (square n)
+   (* n n))
 
-(define (double n) (+ n n))
+(define (double n)
+   (+ n n))
 
 ((comb square double) 5)
-
-"(double 5) = 10"
-"(square (double 5)) = 10 * 10 = 100"
+;; (square(double 5))
+;; (square(10))
+;; 100
