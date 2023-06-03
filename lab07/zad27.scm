650dee62f807200976bc7a7025fd04a83dcacc2d
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 21:42:03 +0200

    zad27

--- lab07/zad27.scm
+++ lab07/zad27.scm
@@ -7,12 +7,4 @@
     (* a b))
   (pp 2))
 
-(p 5)
-
-; (p 5) = 6
-; a przyjmuje wartość 5 w nowym środowisku
-; wywołanie (pp 2)
-; tworzy kolejne środowisko z a = 5
-; set ustawia wartość a na 3
-; potem wywoływane jest (* a b) co daje końcowy wynik 6
-; ostatnia wartość a to 3
+(p 5) ; 6
