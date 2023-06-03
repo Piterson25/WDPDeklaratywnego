a5ad603497eec1b7b1af49e11565935e3379dbf7
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 20:25:22 +0200

    zad19

--- lab05/zad19.scm
+++ lab05/zad19.scm
@@ -1,41 +1,36 @@
 #lang scheme
 
-"19."
-
-(define (append l m)
-  (if (null? l)
-      m
-      (cons (car l) (append (cdr l) m))))
-
 (define (memberTree? element tree)
   (cond ((null? tree) #f)
-        ((eq? (car tree) element ) #t)
-        ((list? (car tree)) (or (memberTree? element (car tree)) (memberTree? element (cdr tree))))
-        (#t (memberTree? element (cdr tree)))))
-
-(memberTree? 5 '(1 2 3 4 5))
+        ((eq? element (car tree)) #t)
+        ((memberTree? element (cadr tree)) #t)
+        ((memberTree? element (caddr tree)) #t)
+        (else #f)))
 
 (define (sumTree tree)
   (cond ((null? tree) 0)
-        ((list? (car tree)) (+ (sumTree (car tree)) (sumTree (cdr tree))))
-        (#t (+ (car tree) (sumTree (cdr tree))))))
+        ((not (pair? tree)) tree)
+        (else (+ (sumTree (car tree))
+                 (sumTree (cadr tree))))))
 
-(sumTree '(1 2 (3 4 (5)) 6))
-
-(define (inorder tree)
-  (cond ((null? tree) '())
-        ((list? (car tree)) (append (inorder (car tree)) (cons (car tree) (inorder (cdr tree)))))
-        (#t (cons (car tree) (inorder (cdr tree))))))
-
-
-(inorder '(1 2 (3 4 (5)) 6))
+(define (inorder t)
+  (cond ((null? t) '())
+        (else (append (inorder (cadr t))
+                      (cons (car t) (inorder (caddr t)))))))
 
 (define (mapTree f tree)
-  (cond ((null? tree) '())
-        ((list? (car tree)) (append (mapTree f (car tree)) (mapTree f (cdr tree))))
-        (else (cons (f (car tree)) (mapTree f (cdr tree))))))
+  (if (null? tree)
+      '()
+      (cons (f (car tree))
+            (cons (mapTree f (cadr tree))
+                  (mapTree f (caddr tree))))))
 
 (define (square x)
     (* x x))
 
-(mapTree square '(1 2 (3 4 (5)) 6))
+(define tree '(1 (2 (4 () ()) (5 () ())) (3 () ())))
+
+(memberTree? 3 tree)
+(sumTree tree)
+(inorder tree)
+(mapTree square tree)
