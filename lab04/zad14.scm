c99114309486fe145f6561e132864f254815bf4c
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 19:56:45 +0200

    zad14

--- lab04/zad14.scm
+++ lab04/zad14.scm
@@ -1,16 +1,8 @@
 #lang scheme
 
-(define (average x y)
-  (/ (+ x y) 2))
-
 (define (root f a b)
-  (let ((fa (f a b))
-        (fb (f b a)))
-    (cond ((< (abs (- a b)) 0.0001) (average a b))
-          ((= fa 0) a)
-          ((= fb 0) b)
-          ((< (* fa (f (average a b) b)) 0)
-           (root f a (average a b)))
-          (else (root f (average a b) b)))))
+  (cond ((and (< (f (/ (+ a b) 2)) 0.00001) (> (f (/ (+ a b) 2)) -0.00001)) (/ (+ a b) 2.0))
+        ((< (f (/ (+ a b) 2)) 0) (root f (/ (+ a b) 2) b))
+        (else (root f a (/ (+ a b) 2)))))
 
-(root average 2 4) ; wynik: 3
+(root (lambda (x) (+ (* 2 x) 4)) -10 10)
