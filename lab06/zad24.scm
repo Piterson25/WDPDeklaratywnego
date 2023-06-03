bcfb40d5698b3528c8edc8c98e2ded552aaa148b
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 21:31:51 +0200

    zad24

--- lab06/zad24.scm
+++ lab06/zad24.scm
@@ -1,30 +1,27 @@
 #lang scheme
 
-"24."
+(define (square a) (* a a))
 
-"a)"
-(define (plus . nums)
-  (apply + nums))
+(define (plus . l)
+(define (help-plus l)
+  (if (empty? l) 
+      0 
+      (+ (car l) (help-plus (cdr l)))))
+  (help-plus l))
 
-(plus 5 4 3 2)
+(define (par . f)
+    (lambda (x) 
+        (reverse (help-par x f '()))
+    )
+)
 
-"b)"
-(define (par . funcs)
-  (lambda (x)
-    (map (lambda (f) (f x)) funcs)))
+(define (help-par x f acc)
+  (if (empty? f) 
+      acc 
+      (help-par x (cdr f) (cons ((car f) x) acc)))
+)
 
-(define (id x)
-  x)
+(define (cube a)
+  (* a a a))
 
-(define (double x)
-  (* x 2))
-
-(define (square x)
-  (* x x))
-
-(define (cube x)
-  (* x x x))
-
-((par id square cube) 3)
-((par double double) 5)
-
+((par identity square cube) 3)
