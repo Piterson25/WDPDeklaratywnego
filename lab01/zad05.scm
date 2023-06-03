3197ef0c333fd7cc6e6aff580d264fd37a5c6b64
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 17:58:19 +0200

    zad05

--- lab01/zad05.scm
+++ lab01/zad05.scm
@@ -1,22 +1,16 @@
 #lang scheme
-(define (new.< x y) (< x y))
-(define (new.> x y) (< y x))
-(define (new.= x y) (and (not(new.> x y)) (not(new.< x y))))
-(define (new.<= x y) (or (< x y) (new.= x y)))
-(define (new.>= x y) (or (> x y) (new.= x y)))
-(define (new.<> x y) (not (new.= x y)))
+
+(define (new.< x y)
+  (< x y))
+(define (new.> x y)
+  (< y x))
+(define (new.= x y)
+  (and (not(new.> x y)) (not(new.< x y))))
 
 (define (same-values? p1 p2 x y)
-  (let ((result1 (p1 x y))
-        (result2 (p2 x y)))
-    (or (and (number? result1)
-             (number? result2)
-             (= result1 result2))
-        (and (not (number? result1))
-             (not (number? result2))
-             (eqv? result1 result2)))))
-
-
+  (if (equal? (p1 x y) (p2 x y))
+      #t
+      #f))
 
 (same-values? = new.= 3 1)
 (same-values? < > 2 3)
