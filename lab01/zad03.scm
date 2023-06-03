7d888566f910677412b0616d94e70bc761be401a
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 17:48:44 +0200

    zad03

--- lab01/zad03.scm
+++ lab01/zad03.scm
@@ -1,9 +1,12 @@
 #lang scheme
-(define (nwd a b) (cond ((= a b) a)
-                        ((> a b) (nwd (- a b) b))
-                        (else (nwd a (- b a)))
-                  ))
-(define (nww a b) (/ (* a b) (nwd a b)))
 
-(nwd 15 1)
+(define (nwd a b) 
+    (cond ((= a b) a)
+        ((> a b) (nwd (- a b) b))
+        (else (nwd a (- b a)))
+    ))
+(define (nww a b) 
+    (/ (* a b) (nwd a b)))
+
+(nwd 15 5)
 (nww 15 5)
