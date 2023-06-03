54c45a8400339d9cab715b196e904c45396726df
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 21:41:16 +0200

    zad26

--- lab07/zad26.scm
+++ lab07/zad26.scm
@@ -6,19 +6,12 @@
 (define (pp x)
    (+ x m))
 
+(p 11) ; 6
+
 (define n 1)
 (define (q n)
   (define (qq x)
     (+ x n))
   (qq 5))
 
-(p 11)
-(q 11)
-
-; zmienne globalne m i n przyjmują wartość 1
-; wykonanie funkcji (p 11)
-; użyte jest m=1 globalne w funkcji (pp) zamiast 11
-; 1 + 5 = 6
-; wykonanie funkcji (q 11)
-; użyty jest argument x=11 do funkcji (qq) wewnątrz (q 11) zamiast n=1
-; 11 + 5 = 6
+(q 11) ; 16
