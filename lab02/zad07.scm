97fc9e35bab4cb78f0b9519f4201379d8fcad582
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 18:14:55 +0200

    zad07

--- lab02/zad07.scm
+++ lab02/zad07.scm
@@ -1,25 +1,33 @@
 #lang scheme
 
-"7 a)"
+;; a)
 
 (define (fibonacci n)
-  (if (<= n 1)
-      n
-      (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
 
 (fibonacci 4)
+;; (+ (fibonacci 3) (fibonacci 2))
+;; (+ (+ (fibonacci 2) (fibonacci 1)) (+ (fibonacci 1) (fibonacci 0)))
+;; (+ (+ (+ (fibonacci 1) (fibonacci 0)) 1) 1)
+;; (+ (+ (+ 1 0) 1) 1)
+;; (+ (+ 1 1) 1)
+;; (+ 2 1)
+;; 3
 
-"7 b)"
+;; b)
 
-(define (fibonacci_help n i acc)
-      (if (= n 0)
-          acc
-          (if (= n 1)
-              i
-      (fibonacci_help (- n 1) acc (+ i acc))))
-      )            
+(define (fibonacci-help n i acc1 acc2)
+  (cond ((= n i) acc1)
+      (else (fibonacci-help n (+ i 1) (+ acc1 acc2) acc1))))
 
-(define (fibonacciRec n)
-      (fibonacci_help n 1 1))
+(define (fibonacci-tail n)
+  (fibonacci-help n 1 1 0))
 
-(fibonacciRec 4)
+(fibonacci-tail 4)
+;; (fibonacci_help 4 1 1 0)
+;; (fibonacci_help 4 2 1 1)
+;; (fibonacci_help 4 3 2 1)
+;; (fibonacci_help 4 4 3 2)
+;; 3
