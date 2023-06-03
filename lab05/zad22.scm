1c55150a1259cf820088c63ac86e3d420ad95b81
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-03 21:14:43 +0200

    zad22

--- lab05/zad22.scm
+++ lab05/zad22.scm
@@ -1,6 +1,5 @@
 #lang scheme
 
-"22."
 (define (fold op init l)
   (if (null? l)
       init
@@ -10,35 +9,27 @@
   (fold * 1 l))
 
 (define (length l)
-  (fold (lambda (x y) (+ y 1)) 0 l))
+  (fold (lambda (x acc) (+ acc 1)) 0 l))
 
 (define (delete x l)
-  (fold (lambda (y z) (if (eq? x y) z (cons y z))) '() l))
+  (fold (lambda (e acc) (if (equal? e x) acc (cons e acc))) '() l))
 
 (define (reverse l)
-  (fold (lambda (x y) (append y (list x))) '() l))
+  (fold (lambda (e acc) (append acc (cons e '()))) '() l))
 
 (define (map f l)
-  (fold (lambda (x y) (cons (f x) y)) '() l))
+  (fold (lambda (e acc) (cons (f e) acc)) '() l))
 
 (define (filter pred l)
-  (fold (lambda (x y) (if (pred x) (cons x y) y)) '() l))
+  (fold (lambda (e acc) (if (pred e) (cons e acc) acc)) '() l))
 
 (define (forall pred l)
-  (fold (lambda (x y) (and y (pred x))) #t l))
+  (fold (lambda (e acc) (and (pred e) acc)) #t l))
 
-(prod '(1 2 3 4))
-
-(length '(1 2 3 4))
-
-(delete 3 '(1 2 3 4))
-
-(reverse '(1 2 3 4))
-
-(map (lambda (x) (* x x)) '(1 2 3 4))
-
-(filter even? '(1 2 3 4))
-
-(forall even? '(1 2 3 4))
-
-(forall even? '(2 4 6 8))
+(prod '(2 3 4 5))
+(length '(1 2 3 4 5))
+(delete 2 '(1 2 3 2 4 2))
+(reverse '(1 2 3 4 5))
+(map (lambda (x) (* x x)) '(1 2 3 4 5))
+(filter even? '(1 2 3 4 5))
+(forall number? '(1 2 3 4 5))
