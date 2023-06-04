306768a668f74c28113123c221b99d5e5d4ee9c3
Piterson25 <piotrmaszczak25@gmail.com>
2023-06-04 11:23:05 +0200

    zad10

--- lab11/zad10.pl
+++ lab11/zad10.pl
@@ -1,11 +1,35 @@
-nth(1,[X],X).
+% a.
+nth(1,[X|_],X).
 nth(N, [_|T], X) :-
     N > 1,
     N1 is N - 1,
     nth(N1, T, X).
 
+% b.
 ordered([]).
 ordered([_]).
-ordered([X,Y|T]) :-
+ordered([X,H|Tail]) :-
+    X =< H,
+    ordered([H|Tail]).
+
+% c.
+mergesort([], []).
+mergesort([X], [X]).
+mergesort(List, Sorted) :-
+    split(List, Left, Right),
+    mergesort(Left, SortedLeft),
+    mergesort(Right, SortedRight),
+    merge(SortedLeft, SortedRight, Sorted).
+
+split([], [], []).
+split([X], [X], []).
+split([X,Y|Tail], [X|Left], [Y|Right]) :- split(Tail, Left, Right).
+
+merge([], L, L).
+merge(L, [], L).
+merge([X|Left], [Y|Right], [X|Result]) :-
     X =< Y,
-    ordered([Y|T]).
+    merge(Left, [Y|Right], Result).
+merge([X|Left], [Y|Right], [Y|Result]) :-
+    X > Y,
+    merge([X|Left], Right, Result).
